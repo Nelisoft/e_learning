f2768cc582f745bd4c41b6d4a2f93946a80f0bdc
Nelisoft <donnelskid287@gmail.com>
2025-03-14 08:46:11 -0700

    corrected2

--- Elearningprj/urls.py
+++ Elearningprj/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path("ckeditor5/", include('django_ckeditor_5.urls')),
     path('userauth/', include('userauth.urls')),
     path('', include('paypal.standard.ipn.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
